a0053ad4f3e6cf128fdb8608054bf00192767256
htoro <htorohn@gmail.com>
2020-08-26 21:57:51 -0600

    Revolution section

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -5,12 +5,16 @@ import Grid from '@material-ui/core/Grid'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
 import useMediaQuery from '@material-ui/core/useMediaQuery'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
 
 import ButtonArrow from './ui/ButtonArrow'
 import animationData from '../animations/landinganimation/data'
 import customSoftwareIcon from '../assets/Custom Software Icon.svg'
 import mobileAppIcon from '../assets/mobileIcon.svg'
 import websitesIcon from '../assets/websiteIcon.svg'
+import revolutionBackground from '../assets/repeatingBackground.svg'
+
 
 const useStyles = makeStyles(theme => ({
     mainContainer: {
@@ -90,6 +94,28 @@ const useStyles = makeStyles(theme => ({
             padding: 25,
             marginTop: '8em',
         }
+    },
+    revolutionBackground: {
+        backgroundImage: `url(${revolutionBackground})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        height: '100%',
+        width: '100%'
+    },
+    revolutionCard: {
+        position: 'absolute',
+        boxShadow: theme.shadows[10],
+        borderRadius: 15,
+        padding: '10em',
+        [theme.breakpoints.down('sm')]: {
+            paddingTop: '8em',
+            paddingBottom: '8em',
+            paddingLeft: 0,
+            paddingRight: 0,
+            borderRadius: 0,
+            width: '100%'
+        }
     }
 }))
 
@@ -142,7 +168,7 @@ const LandingPage = () => {
 
             {/* Custom Software Section */}
             <Grid item className={classes.servicesContainer}>
-                <Grid container justify={matchesSM ? 'center': undefined}>
+                <Grid container justify={matchesSM ? 'center' : undefined}>
                     <Grid item style={{ marginLeft: matchesSM ? 0 : '5em', textAlign: matchesSM ? 'center' : undefined }}>
                         <Typography variant='h4'>
                             Custom Software Development
@@ -166,7 +192,7 @@ const LandingPage = () => {
 
             {/* iOS/Android Section */}
             <Grid item className={classes.servicesContainer}>
-                <Grid container justify={matchesSM ? 'center': 'flex-end'}>
+                <Grid container justify={matchesSM ? 'center' : 'flex-end'}>
                     <Grid item style={{ textAlign: matchesSM ? 'center' : undefined }}>
                         <Typography variant='h4'>
                             iOS/Android App Development
@@ -190,7 +216,7 @@ const LandingPage = () => {
 
             {/* Websites Section */}
             <Grid item className={classes.servicesContainer}>
-                <Grid container justify={matchesSM ? 'center': undefined}>
+                <Grid container justify={matchesSM ? 'center' : undefined}>
                     <Grid item style={{ marginLeft: matchesSM ? 0 : '5em', textAlign: matchesSM ? 'center' : undefined }}>
                         <Typography variant='h4'>
                             Website Development
@@ -209,6 +235,33 @@ const LandingPage = () => {
                     <Grid item>
                         <img className={classes.icon} alt='websites icon' src={websitesIcon} />
                     </Grid>
+                </Grid>
+            </Grid>
+
+            {/* Revolution Section */}
+            <Grid item>
+                <Grid container style={{ height: '100em', marginTop: '12em' }} alignItems='center' justify='center'>
+                    <Card className={classes.revolutionCard}>
+                        <CardContent>
+                            <Grid container direction='column' style={{ textAlign: 'center' }}>
+                                <Grid item>
+                                    <Typography variant='h3' gutterBottom>
+                                        The Revolution
+                                </Typography>
+                                </Grid>
+                                <Grid item>
+                                    <Typography variant='subtitle1'>
+                                        Visionary insights coupled with cutting-edge technology  is a recipe for revolution
+                                </Typography>
+                                    <Button variant='outlined' className={classes.learnButtonHero}>
+                                        <span style={{ marginRight: 10 }}>Learn More</span>
+                                        <ButtonArrow width={15} height={15} fill={theme.palette.common.blue} />
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </CardContent>
+                    </Card>
+                    <div className={classes.revolutionBackground} />
                 </Grid>
             </Grid>
 
